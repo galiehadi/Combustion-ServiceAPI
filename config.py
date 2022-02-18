3d03f349a1e01a11750dda6f13322b8e1178bcc1
ichsansan <ichsan.application@gmail.com>
2022-02-18 11:09:04 +0700

    Change connection configuration on PCT1

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 Setting parameter untuk koneksi ke database.
 '''
 
-_UNIT_CODE_ = "RBG1"
-_UNIT_NAME_ = "PLTU Rembang 10"
+_UNIT_CODE_ = "PCT1"
+_UNIT_NAME_ = "PLTU Pacitan 1"
 _USER_ = 'root'
 _PASS_ = 'P@ssw0rd'
-_IP_ = '192.168.2.11:3306'
-_LOCAL_IP_ = '192.168.2.11'
-_DB_NAME_ = 'db_bat_rmb2'
+_IP_ = '192.168.32.6:3306'
+_LOCAL_IP_ = '192.168.32.6'
+_DB_NAME_ = 'db_bat_pct1'
 
 "Setting parameter nama variabel"
 
