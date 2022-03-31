786616da9d72afa5d764aab5def17ebf5afedda4
ichsansan <ichsan.application@gmail.com>
2022-03-31 15:00:03 +0700

    Updating PCT1

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ _UNIT_CODE_ = "PCT1"
 _UNIT_NAME_ = "PLTU Pacitan 1"
 _USER_ = 'root'
 _PASS_ = 'P@ssw0rd'
-_IP_ = 'localhost:3301' #'192.168.32.6:3306'
+_IP_ = '192.168.32.6:3306'
 _LOCAL_IP_ = '192.168.32.6'
 _DB_NAME_ = 'db_bat_pct1'
 
