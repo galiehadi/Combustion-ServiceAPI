7ba0820ebe02f550828965edd27e8911671123bb
ichsansan <wildanalaziz82_gmail_com@ds-dashboard-vm.asia-southeast2-a.c.fit-authority-236304.internal>
2022-07-22 06:10:13 +0700

    change config to deployment

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ _UNIT_CODE_ = "RBG1"
 _UNIT_NAME_ = "PLTU Rembang Unit 1"
 _USER_ = "bat_copt"
 _PASS_ = "P@ssw0rd"
-_IP_ = "10.7.1.116:33011" # "35.219.48.62" # 
+_IP_ = "192.168.2.10" # "35.219.48.62" # 
 _LOCAL_IP_ = "0.0.0.0:5002"
 _DB_NAME_ = "db_bat_rmb1"
 
