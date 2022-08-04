324cdcd96c8e7dbb23668979d3bc3436fed2fe92
ichsansan <wildanalaziz82_gmail_com@ds-dashboard-vm.asia-southeast2-a.c.fit-authority-236304.internal>
2022-08-04 14:41:48 +0700

    update sh

--- build_docker.sh
+++ build_docker.sh
@@ -7,3 +7,4 @@
 docker build -t services-combustion-rbg1:v1.5 .
 docker image save -o ../services-combustion-rbg1-v1.5.tar services-combustion-rbg1:v1.5
 rsync -Pavre "ssh -p 24019" ../services-combustion-rbg1-v1.5.tar root@10.7.1.116:~/CombustionOpt/zipped/.
+ssh root@10.7.1.116 -p 24019 'cd CombustionOpt/zipped/; ./run_service_tar.sh'
