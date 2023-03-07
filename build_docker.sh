0e20e95a0ae3b9df46a7bbf6e48ff5315ac410ec
ichsansan <wildanalaziz82_gmail_com@ds-dashboard-vm.asia-southeast2-a.c.fit-authority-236304.internal>
2023-03-07 22:44:20 +0700

    Fixing null values on cb_display

--- build_docker.sh
+++ build_docker.sh
@@ -1,9 +1,3 @@
-# docker stop services-combustion-rbg1
-# docker rm services-combustion-rbg1
-# docker image rm  services-combustion-rbg1:v1.5
-# docker image load -i services-combustion-rbg1-v1.5.tar
-# docker run -itd --name services-combustion-rbg1 --restart unless-stopped --memory="300M" -p 0.0.0.0:8083:8083 services-combustion-rbg1:v1.5
-
 clear
 docker build -t services-combustion-pct1:v1.5.5 .
 docker image save -o ../services-combustion-pct1-v1.5.5.tar services-combustion-pct1:v1.5.5
