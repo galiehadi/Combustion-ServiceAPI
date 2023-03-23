f5b22830d9c58387ad755ed56b1ca96f95d961d0
ichsansan <wildanalaziz82_gmail_com@ds-dashboard-vm.asia-southeast2-a.c.fit-authority-236304.internal>
2023-03-23 23:08:59 +0700

    Adding 5min to suggest

--- BackgroundService.py
+++ BackgroundService.py
@@ -221,7 +221,7 @@ def bg_combustion_watchdog_check():
     if not Watchdog_safe and COPT_status == 1:
         logging('Watchdog are disconnected. Turning off COPT ...')
         try:
-            q = f"""UPDATE db_bat_rmb1.tb_bat_raw
+            q = f"""UPDATE tb_bat_raw
                     SET f_value=0,f_updated_at=NOW(),f_date_rec=NOW()
                     WHERE f_address_no=(SELECT f_tag_name FROM tb_tags_read_conf ttrc 
                     WHERE f_description = "{config.DESC_ENABLE_COPT}");"""
@@ -457,6 +457,7 @@ def bg_write_recommendation_to_opc(MAX_BIAS_PERCENTAGE):
         mxv = MAX_BIAS_PERCENTAGE * abs(Recom.loc[i, 'current_value']) / 100
         Recom.loc[i, 'bias_value'] = max(-mxv, Recom.loc[i, 'bias_value'])
         Recom.loc[i, 'bias_value'] = min(mxv, Recom.loc[i, 'bias_value'])
+        
         #cek recomendation except Excess Oxygen Sensor
         if Recom.loc[i,'f_description'] != 'Excess Oxygen Sensor':
             #cek last now - update
