38698530f45ed1b2f9751872c98be503c8ab0e8c
ichsansan <wildanalaziz82_gmail_com@ds-dashboard-vm.asia-southeast2-a.c.fit-authority-236304.internal>
2023-04-10 21:27:24 +0700

    Update local

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,7 @@
 clear
-docker build -t services-combustion-rbg1:v1.5.5 .
-docker image save -o ../services-combustion-rbg1-v1.5.5.tar services-combustion-rbg1:v1.5.5
-ssh ichsan@10.7.1.116 -p 24019 'cp ~/SourceCode/zipped/services-combustion-rbg1-v1.5.4.tar ~/SourceCode/zipped/services-combustion-rbg1-v1.5.5.tar'
-rsync -Pavre "ssh -p 24019" ../services-combustion-rbg1-v1.5.5.tar ichsan@10.7.1.116:~/SourceCode/zipped/.
+docker build -t services-combustion-rbg1:v1.5.6 .
+docker image save -o ../services-combustion-rbg1-v1.5.6.tar services-combustion-rbg1:v1.5.6
+ssh ichsan@10.7.1.116 -p 24019 'cp ~/SourceCode/zipped/services-combustion-rbg1-v1.5.5.tar ~/SourceCode/zipped/services-combustion-rbg1-v1.5.6.tar'
+rsync -Pavre "ssh -p 24019" ../services-combustion-rbg1-v1.5.6.tar ichsan@10.7.1.116:~/SourceCode/zipped/.
 rsync -Pavre "ssh -p 24019" run_service_tar.sh ichsan@10.7.1.116:~/SourceCode/zipped/.
 ssh ichsan@10.7.1.116 -p 24019 'cd SourceCode/zipped/; ./run_service_tar.sh'
