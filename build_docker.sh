9bdf74cd2b1b2eff8c6bc6d6bdd174d377744acf
ichsansan <wildanalaziz82_gmail_com@ds-dashboard-vm.asia-southeast2-a.c.fit-authority-236304.internal>
2023-04-13 04:29:01 +0700

    rbg1

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,7 @@
 clear
-docker build -t services-combustion-rbg2:v1.5.6 .
-docker image save -o ../services-combustion-rbg2-v1.5.6.tar services-combustion-rbg2:v1.5.6
-ssh ichsan@10.7.1.116 -p 24020 'cp ~/SourceCode/zipped/services-combustion-rbg2-v1.5.5.tar ~/SourceCode/zipped/services-combustion-rbg2-v1.5.6.tar'
-rsync -Pavre "ssh -p 24020" ../services-combustion-rbg2-v1.5.6.tar ichsan@10.7.1.116:~/SourceCode/zipped/.
-rsync -Pavre "ssh -p 24020" run_service_tar.sh ichsan@10.7.1.116:~/SourceCode/zipped/.
-ssh ichsan@10.7.1.116 -p 24020 'cd SourceCode/zipped/; ./run_service_tar.sh'
+docker build -t services-combustion-rbg1:v1.5.6 .
+docker image save -o ../services-combustion-rbg1-v1.5.6.tar services-combustion-rbg1:v1.5.6
+ssh ichsan@10.7.1.116 -p 24019 'cp ~/SourceCode/zipped/services-combustion-rbg1-v1.5.5.tar ~/SourceCode/zipped/services-combustion-rbg1-v1.5.6.tar'
+rsync -Pavre "ssh -p 24019" ../services-combustion-rbg1-v1.5.6.tar ichsan@10.7.1.116:~/SourceCode/zipped/.
+rsync -Pavre "ssh -p 24019" run_service_tar.sh ichsan@10.7.1.116:~/SourceCode/zipped/.
+ssh ichsan@10.7.1.116 -p 24019 'cd SourceCode/zipped/; ./run_service_tar.sh'
