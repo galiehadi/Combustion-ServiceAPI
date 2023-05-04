692dc970cfa2f279cabe0509042022fa09ab1877
ichsansan <wildanalaziz82_gmail_com@ds-dashboard-vm.asia-southeast2-a.c.fit-authority-236304.internal>
2023-05-04 16:00:16 +0700

    Update v1.6.1

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,6 @@
 clear
 docker build -t services-combustion-pct1:v1.6.1 .
-docker image save -o ../services-combustion-pct1-v1.5.8.tar services-combustion-pct1:v1.6.1
+docker image save -o ../services-combustion-pct1-v1.6.1.tar services-combustion-pct1:v1.6.1
 ssh ichsan@10.7.1.116 -p 24016 'cp ~/CombustionOpt/zipped/services-combustion-pct1-v1.6.0.tar ~/CombustionOpt/zipped/services-combustion-pct1-v1.6.1.tar'
 rsync -Pavre "ssh -p 24016" ../services-combustion-pct1-v1.6.1.tar ichsan@10.7.1.116:~/CombustionOpt/zipped/.
 rsync -Pavre "ssh -p 24016" run_service_tar.sh ichsan@10.7.1.116:~/CombustionOpt/zipped/.
