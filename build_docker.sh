3cd73667fcdfa8280f9c1f44ad2569af797edfc1
ichsansan <wildanalaziz82_gmail_com@ds-dashboard-vm.asia-southeast2-a.c.fit-authority-236304.internal>
2023-05-29 15:05:02 +0700

    Update v1.8

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,10 @@
+$NV = "v1.8"
+$OV = "v1.7"
+
 clear
-docker build -t services-combustion-pct1:v1.7 .
-docker image save -o ../services-combustion-pct1-v1.7.tar services-combustion-pct1:v1.7
-ssh ichsan@10.7.1.116 -p 24016 'cp ~/CombustionOpt/zipped/services-combustion-pct1-v1.6.1.tar ~/CombustionOpt/zipped/services-combustion-pct1-v1.7.tar'
-rsync -Pavre "ssh -p 24016" ../services-combustion-pct1-v1.7.tar ichsan@10.7.1.116:~/CombustionOpt/zipped/.
+docker build -t services-combustion-pct1:$NV .
+docker image save -o ../services-combustion-pct1-$NV.tar services-combustion-pct1:$NV
+ssh ichsan@10.7.1.116 -p 24016 "cp ~/CombustionOpt/zipped/services-combustion-pct1-$OV.tar ~/CombustionOpt/zipped/services-combustion-pct1-$NV.tar"
+rsync -Pavre "ssh -p 24016" ../services-combustion-pct1-$NV.tar ichsan@10.7.1.116:~/CombustionOpt/zipped/.
 rsync -Pavre "ssh -p 24016" run_service_tar.sh ichsan@10.7.1.116:~/CombustionOpt/zipped/.
-ssh ichsan@10.7.1.116 -p 24016 'cd CombustionOpt/zipped/; ./run_service_tar.sh'
+ssh ichsan@10.7.1.116 -p 24016 "cd CombustionOpt/zipped/; ./run_service_tar.sh"
--- run_service_tar.sh
+++ run_service_tar.sh
@@ -1,5 +1,7 @@
+$NV = "v1.8"
+
 docker stop services-combustion-pct1
 docker rm services-combustion-pct1
-docker image rm  services-combustion-pct1:v1.7
-docker image load -i services-combustion-pct1-v1.7.tar
-docker run -itd --name services-combustion-pct1 --restart unless-stopped --memory="300M" -p 0.0.0.0:8083:8083 services-combustion-pct1:v1.7
+docker image rm  services-combustion-pct1:$NV
+docker image load -i services-combustion-pct1-$NV.tar
+docker run -itd --name services-combustion-pct1 --restart unless-stopped --memory="300M" -p 0.0.0.0:8083:8083 services-combustion-pct1:$NV
