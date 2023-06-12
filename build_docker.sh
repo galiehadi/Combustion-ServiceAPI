28e5858161ea9b4f62a02f00f56f9c65e6cbb8e8
Galieh A Surya <galiehadisurya@gmail.com>
2023-06-12 18:27:42 +0700

    deployment-ptn9

--- build_docker.sh
+++ build_docker.sh
@@ -4,7 +4,7 @@ OV="v1.7"
 clear
 docker build -t services-combustion-pct1:$NV .
 docker image save -o ../services-combustion-pct1-$NV.tar services-combustion-pct1:$NV
-ssh ichsan@10.7.1.116 -p 24016 "cp ~/CombustionOpt/zipped/services-combustion-pct1-$OV.tar ~/CombustionOpt/zipped/services-combustion-pct1-$NV.tar"
-rsync -Pavre "ssh -p 24016" ../services-combustion-pct1-$NV.tar ichsan@10.7.1.116:~/CombustionOpt/zipped/.
-rsync -Pavre "ssh -p 24016" run_service_tar.sh ichsan@10.7.1.116:~/CombustionOpt/zipped/.
-ssh ichsan@10.7.1.116 -p 24016 "cd CombustionOpt/zipped/; ./run_service_tar.sh"
+ssh surya@10.7.1.116 -p 24036 "cp ~/CombustionOpt/zipped/services-combustion-pct1-$OV.tar ~/CombustionOpt/zipped/services-combustion-pct1-$NV.tar"
+rsync -Pavre "ssh -p 24036" ../services-combustion-pct1-$NV.tar surya@10.7.1.116:~/CombustionOpt/zipped/.
+rsync -Pavre "ssh -p 24036" run_service_tar.sh surya@10.7.1.116:~/CombustionOpt/zipped/.
+ssh surya@10.7.1.116 -p 24036 "cd CombustionOpt/zipped/; ./run_service_tar.sh"
